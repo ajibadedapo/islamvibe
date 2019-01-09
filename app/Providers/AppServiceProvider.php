2307460b68a4a6ea57efa48eae32a33b6e388919
ajibadedapo <ajibadehammed@gmail.com>
2019-01-09 04:22:19 +0100

    kjhgvc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Schema::defaultStringLength(191);
+         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
     }
 
     /**
